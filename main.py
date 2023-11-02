17d43ecc338cf357c9c0c3afafac9605671a36a3
A-Hask <ahask93@gmail.com>
2023-11-02 13:15:47 -0400

    fixed ZZZ issue

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-studentLast = input("Please enter the student's last name: ")
+# Student data is collected here
+studentLast = input("Please enter the student's last name here (If you wish to quit this program, enter 'ZZZ'): ")
+# If the user enters ZZZ as the student's last name, the program stops
+if studentLast == "ZZZ":
+    exit()
+
+# If the user enters a last name, the program continues to get the remainder of the information
 studentFirst = input("Please enter the student's first name: ")
 gpa = float(input("Please enter the student's GPA: "))
-
-if studentLast == "ZZZ":
-    quit
 
 if gpa >= 3.50:
     print(studentFirst + " " + studentLast + " is on the Dean's List!")
 
-if gpa >= 3.25:
+elif gpa >= 3.25:
     print(studentFirst + " " + studentLast + " is on the Honor Roll!")
+    
+else:
+    exit()
